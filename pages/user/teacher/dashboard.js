19676b01a03de208c4fb3222023680db4f73a909
SaurabhTiwari4093 <86095513+SaurabhTiwari4093@users.noreply.github.com>
2022-05-18 18:44:38 +0530

    side bar responsive for mobile

--- pages/user/teacher/dashboard.js
+++ pages/user/teacher/dashboard.js
@@ -1,23 +1,17 @@
 import Navbar from "../../../components/navbar";
 import Sidebar from "../../../components/sidebar";
-import { useState ,useEffect} from "react";
+import { useState} from "react";
+import useWindowDimensions from '../../../components/useWindowDimensions';
 
 export default function Dashboard() {
   const [sidebarOpen, setSidebarOpen] = useState(true);
-  const [dashboardWidth,setDashboardWidth]=useState(true);
-  // const [width, setWidth] = useState(window.innerWidth);
+  const { height, width } = useWindowDimensions();
 
-  // useEffect(() => {
-  //   if(width<="768px"){
-  //     //in mobile view open side bar above dashboard
-  //     setDashboardWidth(false);
-  //   }
-  // }, [])
   return (
     <>
       <Navbar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen}/>
       {sidebarOpen?<Sidebar/>:""}
-      <div className="px-4 pt-20 h-screen text-slate-800" style={dashboardWidth&&sidebarOpen?{paddingLeft:"18rem"}:{paddingLeft:"1rem"}}>
+      <div className="px-4 pt-20 h-screen text-slate-800" style={width>="768"&&sidebarOpen?{paddingLeft:"18rem"}:{paddingLeft:"1rem"}}>
         <div className="bg-[#e3f2fd] h-full rounded-t-lg p-4">
           Teacher dashboard
         </div>
