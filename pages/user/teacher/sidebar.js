f18dbbc50bd9d630b41e1f061d0c306a4fd87d2b
SaurabhTiwari4093 <86095513+SaurabhTiwari4093@users.noreply.github.com>
2022-05-18 20:00:18 +0530

    hide scroll bar

--- pages/user/teacher/sidebar.js
+++ pages/user/teacher/sidebar.js
@@ -1,10 +1,11 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faGauge,faUsers} from '@fortawesome/free-solid-svg-icons';
+import styles from "../../../styles/Home.module.css";
 
 export default function Sidebar() {
   return (
     <div className='w-72 pt-20 h-screen fixed left-0 bg-white z-10'>
-        <div className="flex flex-col gap-4 p-4 font-medium text-sm text-slate-600 h-full overflow-y-auto">
+        <div className="flex flex-col gap-4 p-4 font-medium text-sm text-slate-600 h-full overflow-y-auto" id={styles.noScrollBar}>
 
           <div className='border-b p-2'>
             <div className='text-slate-900 mb-3 font-semibold'>Dashboard</div>
