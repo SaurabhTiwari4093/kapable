3f90d11d43e070bd29b8d445080afb3aaf798d48
SaurabhTiwari4093 <86095513+SaurabhTiwari4093@users.noreply.github.com>
2022-05-20 03:46:22 +0530

    add month in calender

--- components/calender.js
+++ components/calender.js
@@ -1,22 +1,38 @@
 import FullCalendar from "@fullcalendar/react";
 import interactionPlugin from "@fullcalendar/interaction";
+import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from "@fullcalendar/timegrid";
 import { useRef } from "react";
 
 const Calendar = () => {
   const calendarRef = useRef(null);
-  
+
   return (
     <div className="bg-white p-6 rounded-lg border border-[#bee1fa]">
         <FullCalendar
           innerRef={calendarRef}
-          plugins={[timeGridPlugin, interactionPlugin]}
+          plugins={[timeGridPlugin,dayGridPlugin, interactionPlugin]}
+          headerToolbar={{
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+          }}
           editable
           selectable
           height={550}
           events={[
-            { title: 'Event 1', date: '2022-05-19' },
-            { title: 'Event 2', date: '2022-05-21' }
+            {
+              title  : 'event1',
+              start  : '2022-05-17T02:54:00',
+            },
+            {
+              title  : 'event2',
+              start  : '2022-05-23T11:00:00',
+            },
+            {
+              title  : 'event3',
+              start  : '2022-05-21T12:30:00',
+            }
           ]}
         />
     </div>
