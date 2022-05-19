a170d015c2b0bd7b7bcee214ff3a4f46941e4c91
SaurabhTiwari4093 <86095513+SaurabhTiwari4093@users.noreply.github.com>
2022-05-19 21:50:35 +0530

    calender

--- pages/user/teacher/takeClass/schedule.js
+++ pages/user/teacher/takeClass/schedule.js
@@ -2,6 +2,7 @@ import Navbar from "../../../../components/navbar";
 import Sidebar from "../sidebar";
 import { useState} from "react";
 import useWindowDimensions from '../../../../components/useWindowDimensions';
+import Calender from "../../../../components/calender";
 
 export default function Dashboard() {
   const [sidebarOpen, setSidebarOpen] = useState(true);
@@ -14,6 +15,9 @@ export default function Dashboard() {
       <div className="px-4 pt-20 h-screen text-slate-800" style={width>="768"&&sidebarOpen?{paddingLeft:"18rem"}:{paddingLeft:"1rem"}}>
         <div className="bg-[#e3f2fd] h-full rounded-t-lg p-4">
           Schedule
+          <div>
+            <Calender/>
+          </div>
         </div>
       </div>
     </>
