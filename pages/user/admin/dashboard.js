2d34f83517c5d2af21b9ca79e2339d6466c4f845
SaurabhTiwari4093 <86095513+SaurabhTiwari4093@users.noreply.github.com>
2022-05-21 01:11:48 +0530

    updated sidebar

--- pages/user/admin/dashboard.js
+++ pages/user/admin/dashboard.js
@@ -1,8 +1,7 @@
 import Navbar from "../../../components/navbar";
-import Sidebar from "../../../components/teacher/sidebar";
+import Sidebar from "../../../components/admin/sidebar";
 import { useState} from "react";
 import useWindowDimensions from '../../../components/useWindowDimensions';
-import Calender from "../../../components/teacher/takeClass/calender";
 import styles from "../../../styles/Home.module.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faCalendarWeek} from '@fortawesome/free-solid-svg-icons';
@@ -23,7 +22,7 @@ export default function Dashboard() {
               <div>Admin Dashboard</div>
             </div>
             <div className="p-6">
-              <Calender/>
+              
             </div>
           </div>
         </div>
