e1c160e15e4ba1724d91b2b825514c8f6648721e
SaurabhTiwari4093 <86095513+SaurabhTiwari4093@users.noreply.github.com>
2022-05-25 17:05:18 +0530

    side bar add activity

--- components/counsellor/curriculum/addActivity.js
+++ components/counsellor/curriculum/addActivity.js
@@ -9,10 +9,10 @@ export default function AddActivity(props) {
 
   return (
     <div className="w-full grid grid-cols-5 h-screen fixed right-0 top-0 z-20 text-slate-800">
-        <div className="bg-opacity-50 col-span-2 bg-black">
+        <div className="bg-opacity-50 col-span-1 md:col-span-2 bg-black">
 
         </div>
-        <div className="col-span-3 bg-white">
+        <div className="col-span-4 md:col-span-3 bg-white">
             <div className="bg-[#e3f2fd] p-4 md:p-6 flex items-center">
                 <div><button className="py-1 px-2 text-xl rounded-lg bg-[#1e88e5] text-white" onClick={closeAddActivitySidebar}>X</button></div>
                 <div className="text-center font-bold text-base md:text-xl w-full">Add New Activity</div>
