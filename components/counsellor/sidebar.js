4a36ecd52d1f0bf04572351528a6c049517cfb45
SaurabhTiwari4093 <86095513+SaurabhTiwari4093@users.noreply.github.com>
2022-05-25 14:40:39 +0530

    added new pages

--- components/counsellor/sidebar.js
+++ components/counsellor/sidebar.js
@@ -71,6 +71,11 @@ export default function Sidebar() {
               <div className="flex flex-col px-6 text-sm mt-1 font-normal" style={{display:displayCurriculum}}>
                  <Link href="/user/counsellor/curriculum/curriculumLibrary"><li className='cursor-pointer px-3 py-1.5 rounded-lg hover:text-[#673ab7] hover:font-semibold'>Curriculum Library</li></Link>
                  <Link href="/user/counsellor/curriculum/createCurriculum"><li className='cursor-pointer px-3 py-1.5 rounded-lg hover:text-[#673ab7] hover:font-semibold'>Create Curriculum</li></Link>
+                 <Link href="/user/counsellor/curriculum/activities"><li className='cursor-pointer px-3 py-1.5 rounded-lg hover:text-[#673ab7] hover:font-semibold'>Activities</li></Link>
+                 <Link href="/user/counsellor/curriculum/categories"><li className='cursor-pointer px-3 py-1.5 rounded-lg hover:text-[#673ab7] hover:font-semibold'>Categories</li></Link>
+                 <Link href="/user/counsellor/curriculum/subcategories"><li className='cursor-pointer px-3 py-1.5 rounded-lg hover:text-[#673ab7] hover:font-semibold'>SubCategories</li></Link>
+                 <Link href="/user/counsellor/curriculum/props"><li className='cursor-pointer px-3 py-1.5 rounded-lg hover:text-[#673ab7] hover:font-semibold'>Props</li></Link>
+                 <Link href="/user/counsellor/curriculum/formats"><li className='cursor-pointer px-3 py-1.5 rounded-lg hover:text-[#673ab7] hover:font-semibold'>Formats</li></Link>
               </div>
             </div>
 
---
+++ pages/user/counsellor/curriculum/props.js
@@ -0,0 +1,34 @@
+import Navbar from "../../../../components/navbar";
+import Sidebar from "../../../../components/counsellor/sidebar";
+import { useState} from "react";
+import useWindowDimensions from '../../../../components/useWindowDimensions';
+import styles from "../../../../styles/Home.module.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faListCheck, faPlusCircle } from '@fortawesome/free-solid-svg-icons';
+import Image from "next/image";
+import Owl from "../../../../assets/owl.png"
+
+export default function Props() {
+  const [sidebarOpen, setSidebarOpen] = useState(true);
+  const { width } = useWindowDimensions();
+
+  return (
+    <>
+      <Navbar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen}/>
+      {sidebarOpen?<Sidebar/>:""}
+      <div className="px-4 pt-20 h-screen text-slate-800" style={width>="768"&&sidebarOpen?{paddingLeft:"18rem"}:{paddingLeft:"1rem"}}>
+        <div className="bg-[#e3f2fd] h-full rounded-t-lg p-4 md:p-6 overflow-y-auto" id={styles.noScrollBar}>
+          <div className="bg-white rounded-lg border border-[#bee1fa] h-full">
+            <div className="p-4 md:p-6 font-semibold text-lg border-b flex items-center gap-3">
+              <div className="flex items-center"><FontAwesomeIcon icon={faListCheck}/></div>
+              <div>Props</div>
+            </div>
+            <div className="p-4 md:p-6 flex items-center justify-center h-full text-center">
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
