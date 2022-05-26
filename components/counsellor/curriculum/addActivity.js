094b5261297f965b037517fea332c35e1896c3ac
SaurabhTiwari4093 <86095513+SaurabhTiwari4093@users.noreply.github.com>
2022-05-26 10:10:00 +0530

    updated add activity

--- components/counsellor/curriculum/addActivity.js
+++ components/counsellor/curriculum/addActivity.js
@@ -26,11 +26,11 @@ export default function AddActivity(props) {
         <div className="bg-opacity-50 col-span-1 md:col-span-2 bg-black">
         </div>
         <div className="col-span-4 md:col-span-3 bg-white">
-            <div className="bg-[#e3f2fd] p-4 md:p-6 flex items-center">
+            <div className="bg-[#e3f2fd] fixed top-0 w-full p-4 md:p-6 flex items-center justify-start gap-4 md:gap-6">
                 <div><button className="py-1 px-3 text-xl rounded-lg bg-[#673ab7] hover:bg-[#563199] text-white" onClick={closeAddActivitySidebar}>X</button></div>
-                <div className="text-center font-bold text-base md:text-xl w-full">Add New Activity</div>
+                <div className="text-center font-bold text-base md:text-xl">Add New Activity</div>
             </div>
-            <div className="p-4 md:p-6">
+            <div className="p-4 pt-24 md:p-6 md:pt-24">
               <div className="border-b-2 py-3 mb-4 md:mb-6">
                  <div className="text-base md:text-xl font-semibold my-1">
                    Basics
@@ -115,6 +115,35 @@ export default function AddActivity(props) {
                           </select>
                         </label>
                       </div>
+                    </div>
+                  </div>
+                  <div className="col-span-4 rounded-lg bg-[#f6f7f8] p-4 md:p-6 grid grid-cols-2 gap-4 md:gap-6 font-semibold">
+                    <div className="col-span-1">
+                      <label>Picture Based
+                        <select className="w-full p-2 border border-slate-200 rounded-lg outline-none text-slate-600">
+                          <option>Select</option>
+                          <option>Yes</option>
+                          <option>No</option>
+                        </select>
+                      </label>
+                    </div>
+                    <div className="col-span-1">
+                      <label>Formats
+                        <select className="w-full p-2 border border-slate-200 rounded-lg outline-none text-slate-600">
+                          <option>Select</option>
+                          <option>Option 1</option>
+                          <option>Option 2</option>
+                        </select>
+                      </label>
+                    </div>
+                    <div className="col-span-2">
+                      <label>Props
+                        <select className="w-full p-2 border border-slate-200 rounded-lg outline-none text-slate-600">
+                          <option>Select</option>
+                          <option>Option 1</option>
+                          <option>Option 2</option>
+                        </select>
+                      </label>
                     </div>
                   </div>
               </div>
